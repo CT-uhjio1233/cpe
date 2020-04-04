233e5a0cbb5f8ab26fbfe64bf88e15b9ed693cf7
CT-uhjio1233 <uhjio1233@gmail.com>
2020-04-04 16:53:05 +0800

    cpe1-11

---
+++ cpestart1of11.java
@@ -0,0 +1,41 @@
+package cpe;
+
+import java.util.*;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+public class cpestart1of11{
+    public static void main(String[] args){
+        Scanner cin=new Scanner(System.in);
+        while(cin.hasNext()){
+        String a=cin.nextLine();
+        String b=cin.nextLine();
+        int[] tab1=new int[26];
+        int[] tab2=new int[26] ;
+        Arrays.fill(tab1,0);
+        Arrays.fill(tab2,0);
+
+        for(int i=0;i<a.length();i++){
+            int d=a.charAt(i);
+            if(d>=97&&d<=122){
+            tab1[d-97]++;
+            }
+        }
+        for(int i=0;i<b.length();i++){
+            int d=b.charAt(i);
+            if(d>=97&&d<=122){
+            tab2[d-97]++;
+            }
+        }
+
+        for(int i=0;i<=26;i++){
+            int min=Math.min(tab1[i], tab2[i]);
+            for(int j=0;j<min;j++){
+                System.out.print((char)(i+97));
+            } 
+        }
+        System.out.println();
+        }
+    }
+}
+       
