4ba1ba413dc048c87564cf8dfdb1241340658878
CT-uhjio1233 <uhjio1233@gmail.com>
2020-04-05 19:13:20 +0800

    Addcpe12

---
+++ cpestart1of12.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+class uva490{
+	
+public static void main(String[] args){
+    Scanner cin=new Scanner(System.in);
+    Vector<String> v=new Vector<String>();
+    int max=0;
+    int cases=0;
+    while(cin.hasNextLine()){
+        String a=cin.nextLine();
+        if(max<a.length())max=a.length();
+        v.add(a);
+        cases++;
+    }
+    int count=0;
+    for(int i=0;i<max;i++){
+        for(int j=cases-1;j>=0;j--){
+            if(i<v.get(j).length()){
+                while(count>0){
+                    System.out.print(" ");
+                    count--;
+                }
+                System.out.print(v.get(j).charAt(i));
+                count=0;
+            }else{
+                count++;
+            }
+        }
+        System.out.println();
+    }
+}
+}
