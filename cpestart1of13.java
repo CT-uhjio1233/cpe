2e3048b3e056f1ea73976a5d7ca42dc199ad348f
CT-uhjio1233 <uhjio1233@gmail.com>
2020-04-08 08:49:03 +0800

    cpe-13_finished

--- cpestart1of13.java
+++ cpestart1of13.java
@@ -1,19 +1,29 @@
 
 import java.util.Scanner;
-import java.util.Vector;
 
 public class cpestart1of13{
     public static void main(String[] args){
         Scanner cin=new Scanner(System.in);
-        
+        int x=0;
         while(cin.hasNext()){
             String a=cin.nextLine();
-            if(a.equals("end-of-file")){
+            if(a=="end-of-file"){
                 break;
-            }else{
-                 System.out.println(a.replaceAll("\"","\'\'"));
-            }   
-        }
-        cin.close();
+            }
+           
+            for(int i=0;i<a.length();i++){
+                ;
+                if(a.charAt(i)=='"'&&x%2==0){
+                    System.out.print("``");
+                    x++;
+                }else if(a.charAt(i)=='"'&&x%2==1){
+                    System.out.print("\'\'");
+                    x++;
+                }else{
+                    System.out.print(a.charAt(i));
+                }
+            }
+            System.out.println();
+        } cin.close();
     }
 }
